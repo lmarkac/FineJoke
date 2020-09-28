3af8d194645f65900e2a964b686f6e23d0ab1d7e
Luka Markač <luka.markac@ericsson.com>
2020-09-29 00:27:53 +0200

    Forgot to commit application.properties, index.html and IndexPageController.java.

---
+++ src/main/java/com/hr/luka/finejoke/controllers/IndexPageController.java
@@ -0,0 +1,34 @@
+package com.hr.luka.finejoke.controllers;
+
+import com.hr.luka.finejoke.entity.Category;
+import com.hr.luka.finejoke.entity.Joke;
+import com.hr.luka.finejoke.service.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class IndexPageController {
+
+    @Autowired
+    JokeService jokeService;
+    
+    public IndexPageController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @GetMapping("/")
+    public String index(Model model){
+
+        List<Joke> allJokes = jokeService.getAllJokes();
+        List<Category> allCategories = jokeService.getAllCategories();
+
+        model.addAttribute("allJokes", allJokes);
+        model.addAttribute("allCategories", allCategories);
+
+        return "index";
+    }
+}
