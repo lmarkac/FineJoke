d028e0d643c1fe2d9653d59a4ba39b8a912c5839
Luka Markač <luka.markac@ericsson.com>
2020-10-13 14:13:40 +0200

    Test.

--- src/main/java/com/hr/luka/finejoke/controllers/LikeAndDislikeRestController.java
+++ src/main/java/com/hr/luka/finejoke/controllers/LikeAndDislikeRestController.java
@@ -1,13 +1,17 @@
 package com.hr.luka.finejoke.controllers;
 
 import com.hr.luka.finejoke.service.JokeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/")
+
 public class LikeAndDislikeRestController {
 
+    private static final Logger logger = LoggerFactory.getLogger(LikeAndDislikeRestController.class);
     JokeService jokeService;
 
     @Autowired
@@ -17,12 +21,13 @@ public class LikeAndDislikeRestController {
 
     @PutMapping(path = "/like/{id}")
     public void like(@PathVariable("id") int id){
-        System.out.println("Uso");
+        logger.info("like");
         jokeService.like(id);
     }
 
     @PutMapping(path = "/dislike/{id}")
     public void dislike(@PathVariable("id") int id){
+        logger.info("dislike");
         jokeService.dislike(id);
     }
 
