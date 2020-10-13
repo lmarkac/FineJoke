c03951ce2ef581d52a721a6021130fb429e9663f
Luka Markač <luka.markac@ericsson.com>
2020-10-13 14:20:21 +0200

    Test.

--- src/main/resources/static/js/action.js
+++ src/main/resources/static/js/action.js
@@ -1,15 +1,13 @@
 function likeAJoke(id) {
     let xhr = new XMLHttpRequest();
-    console.log("im liking it")
-    xhr.open('PUT', "http://localhost:8080/like/" + id, true);
+    xhr.open('PUT', "/like/" + id, true);
     xhr.send();
     document.location.reload();
 }
 
 function dislikeAJoke(id) {
     let xhr = new XMLHttpRequest();
-    console.log("im disliking it")
-    xhr.open('PUT', "http://localhost:8080/dislike/" + id, true);
+    xhr.open('PUT', "/dislike/" + id, true);
     xhr.send();
     document.location.reload();
 }
